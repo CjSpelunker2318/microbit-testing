7c0e6ebf0d8326c65b4a60ad42659b9f6e5c807c
Caroline Ca <69858861+CjSpelunker2318@users.noreply.github.com>
2020-11-24 11:22:36 -0500

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -3,7 +3,8 @@ basic.showString("Z-SENDER")
 input.setAccelerometerRange(AcceleratorRange.EightG)
 radio.setGroup(1)
 function on_forever() {
-    radio.sendNumber(input.acceleration(Dimension.Z))
+    //  radio.send_number(input.acceleration(Dimension.Z))
+    radio.sendValue("Z value", input.acceleration(Dimension.Z))
 }
 
 on_forever()
