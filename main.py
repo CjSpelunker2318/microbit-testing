8b05aa09db917b51ea5996e668e20846421739c3
Caroline Ca <69858861+CjSpelunker2318@users.noreply.github.com>
2020-12-01 10:30:48 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ radio.set_group(56)
 
 def on_forever():
     led.toggle(4, 4)
-    # radio.send_number(input.acceleration(Dimension.X))
-    # radio.send_number(input.acceleration(Dimension.Y))
-    # radio.send_number(input.acceleration(Dimension.Z))
     radio.send_value("x", input.acceleration(Dimension.X))
     radio.send_value("y", input.acceleration(Dimension.Y))
     radio.send_value("z", input.acceleration(Dimension.Z))
