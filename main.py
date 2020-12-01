a07a4f22a14d112ca8214c78db874bcddced8c3d
Caroline Ca <69858861+CjSpelunker2318@users.noreply.github.com>
2020-12-01 09:53:27 -0500

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # Transmitter
 basic.show_string("SENDER")
-input.set_accelerometer_range(AcceleratorRange.EIGHT_G)
+# input.set_accelerometer_range(AcceleratorRange.EIGHT_G)
 radio.set_group(56)
+# radio.send_value("Z value", input.acceleration(Dimension.Z))
 
 def on_forever():
     led.toggle(4, 4)
     radio.send_number(input.acceleration(Dimension.Z))
-    # radio.send_value("Z value", input.acceleration(Dimension.Z))
-on_forever()
+    basic.pause(500)
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,11 +1,10 @@
 //  Transmitter
 basic.showString("SENDER")
-input.setAccelerometerRange(AcceleratorRange.EightG)
+//  input.set_accelerometer_range(AcceleratorRange.EIGHT_G)
 radio.setGroup(56)
-function on_forever() {
+//  radio.send_value("Z value", input.acceleration(Dimension.Z))
+basic.forever(function on_forever() {
     led.toggle(4, 4)
     radio.sendNumber(input.acceleration(Dimension.Z))
-}
-
-//  radio.send_value("Z value", input.acceleration(Dimension.Z))
-on_forever()
+    basic.pause(500)
+})
