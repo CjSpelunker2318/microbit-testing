393246717d82b5b47db7e88cadd33df26be6dc19
Caroline Ca <69858861+CjSpelunker2318@users.noreply.github.com>
2020-12-04 10:46:04 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -7,5 +7,14 @@ def on_forever():
     radio.send_value("x", input.acceleration(Dimension.X))
     radio.send_value("y", input.acceleration(Dimension.Y))
     radio.send_value("z", input.acceleration(Dimension.Z))
-    basic.pause(200)
+    basic.pause(150)
 basic.forever(on_forever)
+
+# Compass 
+from microbit import *
+input.calibrate_compass()
+
+def on_forever2():
+    radio.send_value("degree", input.compass_heading())
+    basic.pause(100)
+basic.forever(on_forever2)
--- main.ts
+++ main.ts
@@ -6,5 +6,11 @@ basic.forever(function on_forever() {
     radio.sendValue("x", input.acceleration(Dimension.X))
     radio.sendValue("y", input.acceleration(Dimension.Y))
     radio.sendValue("z", input.acceleration(Dimension.Z))
-    basic.pause(200)
+    basic.pause(150)
+})
+//  Compass 
+input.calibrateCompass()
+basic.forever(function on_forever2() {
+    radio.sendValue("degree", input.compassHeading())
+    basic.pause(100)
 })
