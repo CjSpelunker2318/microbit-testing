121956becb9c3145370cd8f30051a9ee48ffeded
Caroline Ca <69858861+CjSpelunker2318@users.noreply.github.com>
2020-12-08 09:45:16 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 # Transmitter (sender)
 basic.show_string("SENDER")
 radio.set_group(56)
+input.calibrate_compass()
 
 def on_forever():
     led.toggle(4, 4)
-    radio.send_value("x", input.acceleration(Dimension.X))
-    radio.send_value("y", input.acceleration(Dimension.Y))
-    radio.send_value("z", input.acceleration(Dimension.Z))
+    x = input.acceleration(Dimension.X)
+    y = input.acceleration(Dimension.Y)
+    z = input.acceleration(Dimension.Z)
+    radio.send_value("x", x)
+    radio.send_value("y", y)
+    radio.send_value("z", z)
+    radio.send_value("xz", strength2D(x, z))
+    radio.send_value("yz", strength2D(y, z))
+    radio.send_value("degree", input.compass_heading())
     basic.pause(150)
 basic.forever(on_forever)
 
-# Compass 
-from microbit import *
-input.calibrate_compass()
-
-def on_forever2():
-    radio.send_value("degree", input.compass_heading())
-    basic.pause(100)
-basic.forever(on_forever2)
+# strength2D
+def strength2D(a, b):
+    return Math.sqrt(a**2 + b**2)
--- main.ts
+++ main.ts
@@ -1,16 +1,22 @@
 //  Transmitter (sender)
 basic.showString("SENDER")
 radio.setGroup(56)
+input.calibrateCompass()
 basic.forever(function on_forever() {
     led.toggle(4, 4)
-    radio.sendValue("x", input.acceleration(Dimension.X))
-    radio.sendValue("y", input.acceleration(Dimension.Y))
-    radio.sendValue("z", input.acceleration(Dimension.Z))
+    let x = input.acceleration(Dimension.X)
+    let y = input.acceleration(Dimension.Y)
+    let z = input.acceleration(Dimension.Z)
+    radio.sendValue("x", x)
+    radio.sendValue("y", y)
+    radio.sendValue("z", z)
+    radio.sendValue("xz", strength2D(x, z))
+    radio.sendValue("yz", strength2D(y, z))
+    radio.sendValue("degree", input.compassHeading())
     basic.pause(150)
 })
-//  Compass 
-input.calibrateCompass()
-basic.forever(function on_forever2() {
-    radio.sendValue("degree", input.compassHeading())
-    basic.pause(100)
-})
+//  strength2D
+function strength2D(a: number, b: number): number {
+    return Math.sqrt(a ** 2 + b ** 2)
+}
+
